da98a3261a9f245f169ac1a8f73f81e655c31cd6
Henry Philpotts <hphilpotts@gmail.com>
2023-06-24 13:25:05 +0100

    Shiny mobile responsive MUI AppBar added, paths not yet updated from root.

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,18 +1,92 @@
+import * as React from 'react'
+
 import { AppBar, Button, Toolbar } from '@mui/material'
+import Box from '@mui/material/Box'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Menu from '@mui/material/Menu'
+import MenuIcon from '@mui/icons-material/Menu'
+import Container from '@mui/material/Container'
+import MenuItem from '@mui/material/MenuItem'
+
 import { useNavigate } from 'react-router-dom'
 
+const pages = [{name: 'Home', path: '/'}, {name: 'New', path: '/'}, {name: 'Login', path: '/'}]
+
 export const Navbar = () => {
-    const navigate = useNavigate()
 
+  const navigate = useNavigate()
+  const handleClick = (path: string) => navigate(path)
 
-    const handleClick = (path: string) => navigate(path)
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget)
+  }
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null)
+  }
+
+  const clickHandler = (path:string) => {
+    handleClick(path)
+    handleCloseNavMenu()
+  }
 
   return (
-    <AppBar color="secondary" variant="outlined"> 
-        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <Button onClick={() => handleClick('/')}variant="text" color="inherit">Home</Button>
-            <Button variant="text" color="inherit">Login</Button>
+    <AppBar position="static" color="secondary" variant="outlined">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page.name} onClick={() => clickHandler(page.path)}>
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.name}
+                variant="text" 
+                color="inherit"
+                onClick={() => clickHandler(page.path)}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
         </Toolbar>
+      </Container>
     </AppBar>
   )
 }
